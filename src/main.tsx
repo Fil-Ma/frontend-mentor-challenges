f24e09dcd8d918fb9170dca3f788bf8330701bcc
Fil-Ma <filippo.mazzalai@gmail.com>
2025-12-30 17:31:56 +0100

    resolved major vulnerabilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { scan } from "react-scan";
+// import { scan } from "react-scan";
 
-scan({
-  enabled: false, // Enable or disable React Scan
-});
+// scan({
+//   enabled: false, // Enable or disable React Scan
+// });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
